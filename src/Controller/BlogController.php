2e4e1df02044a989521845be58d1d9b01ef9b751
Hubert Wnukiewicz <hwnukiewicz@gmail.com>
2021-08-28 21:11:07 +0200

    creation of view for displaying blog details

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,13 +9,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'blog')]
+//    #[Route('/blog', name: 'blog')]
+    /**
+     * @Route("/blog", name="blog")
+     */
     public function index(BlogRepository $blogRepository) : Response
     {
         return $this->render(
             'blog/index.html.twig',
             [
                 'blogs' => $blogRepository->findAll(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/blog/details/{id}", name="blog_details")
+     */
+    public function getDetails(BlogRepository $blogRepository, string $id) : Response
+    {
+        return $this->render(
+            'blog/details.html.twig',
+            [
+                'blog' => $blogRepository->find($id),
             ]
         );
     }
